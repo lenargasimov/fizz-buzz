d6d8e58055541242eccd4a64a2e02372dd3c9152
lenargasimov <lenargasimov@duck.com>
2022-04-20 12:40:46 +0300

    refactor: changed patter matching

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 def fizz_buzz():
     for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz")
-        elif number % 3 == 0:
-            print("Fizz")
-        elif number % 5 == 0:
-            print("Buzz")
-        else:
-            print(number)
+        divisible3 = number % 3
+        divisible5 = number % 5
+
+        match (divisible3, divisible5):
+            case (0, 0):
+                print("FizzBuzz")
+            case (0, _):
+                print("Fizz")
+            case (_, 0):
+                print("Buzz")
+            case _:
+                print(number)
 
 
 fizz_buzz()
